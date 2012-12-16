ac434a525c404126c7c4039ff87bea6623a7f40d
illusioni <klaus@illusioni.de>
2012-12-16 18:47:58 +0100

    updated backbone.js for listenTo support, removed surrounding tr tag from template

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,22 +1,11 @@
 //most stuff here will be replaced by better code
 //and/or go into individual files later
-//right now I am focussed on the model part and consciously choose
-//to hack everything else
 
 $(function() {
 	
 	var Events = new EventList;
-	
-	//hack for displaying an EventList
-	//uses Backbone's event mechanism
-	//TODO: remove - we will use views instead
 		
 	Events.on("add", function(item) {
-//TODO rm
-//		var source = $("#item-template").html();
-//		var template = Handlebars.compile(source);
-//		$('#events-table').append(template(item.attributes));
-		
 		$('#log-el').append('Item added: ' + JSON.stringify(item) + '\n');
 	});
 	
@@ -26,6 +15,8 @@ $(function() {
 	var EventItemView = Backbone.View.extend({
 		
 		template: Handlebars.compile($("#item-template").html()),
+		
+		tagName: "tr",
 		
 		initialize: function() {
 			this.listenTo(this.model, 'change', this.render);
@@ -41,23 +32,20 @@ $(function() {
 	
 	var EventListView = Backbone.View.extend({
 		
+		el: $("#content"),
+		
 		initialize: function() {
-			alert('EventListView.initialize()');
-//TODO		this.listenTo(Events, 'add', this.addEvent);
+			this.listenTo(Events, "add", this.addEvent);
 		},
 		
 		addEvent: function(newEvent) {
-			alert('EventListView.addEvent()');
-//TODO		alert('EventListView.addEvent(' + JSON.stringify(newEvent.toJSON()) + ')');
-//			var view = new EventItemView({model: newEvent});
-//			this.$("#events-table").append(view.render().el);
+			var view = new EventItemView({model: newEvent});
+			this.$("#events-table").append(view.render().el);
 		}
 		
 	});
 	
-	alert('a');
 	var EventsView = new EventListView;
-	alert('b');
 	
 	
 	Events.add(new Event({date: '2012-12-01', attending: ['Frank', 'Verena', 'Ada',  'Klaus', 'Dan'], absent: ['Saskia', 'Michi']}));
